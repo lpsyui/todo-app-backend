e29d71b056633a7d83e504903c7095a0cef58a4d
lpsyui <lpsyui@yahoo.co.jp>
2023-12-29 23:27:25 +0900

    update 登録API

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
+
+// ToDo構造体の定義
+type ToDo struct {
+	ID      string `json:"id"`
+	Content string `json:"content"`
+}
+
+// ToDoリストの初期化
+var todoList []ToDo
 
 func main() {
 	// ルーターの初期化
@@ -15,6 +25,7 @@ func main() {
 
 	// ルートハンドラの設定
 	r.HandleFunc("/api/hello", helloHandler).Methods("GET")
+	r.HandleFunc("/api/todos", createToDoHandler).Methods("POST")
 
 	// CORSミドルウェアの追加
 	handler := cors.Default().Handler(r)
@@ -29,4 +40,28 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"message": "TODO１：勉強する〜"})
+}
+
+func createToDoHandler(w http.ResponseWriter, r *http.Request) {
+	// リクエストボディからToDoを読み取る
+	var todo ToDo
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// ToDoにIDを追加してリストに追加
+	todo.ID = generateID()
+	todoList = append(todoList, todo)
+
+	// レスポンスの書き込み
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(todo)
+}
+
+// generateIDは簡易的なID生成関数
+func generateID() string {
+	return time.Now().Format("20060102150405")
 }
